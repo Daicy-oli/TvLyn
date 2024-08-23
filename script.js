fdd4a8fecce73862d24e5ff2f2726dda57693663
Daicy-oli <daicyolivares@gmail.com>
2024-08-23 16:10:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -55,12 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         // Redirigir si hay un cambio en el tamaño de la ventana y no es un dispositivo móvil
-        if (!detectMobileDevice()) {
-            if (resizeTimeout) clearTimeout(resizeTimeout);
-            resizeTimeout = setTimeout(() => {
-                window.location.href = "error.php";
-            }, 100); // Espera 100ms antes de redirigir
-        }
+     
     }
 
     // Función para manejar el cambio en el estado de pantalla completa
@@ -209,40 +204,9 @@ document.getElementById('close-iframefull').addEventListener('click', () => {
 
 ///////////////////////////////////////
 
-function blockDevTools(e) {
-    if (
-        e.keyCode === 123 || // F12
-        (e.ctrlKey && e.shiftKey && e.keyCode === 73) || // Ctrl+Shift+I
-        (e.ctrlKey && e.shiftKey && e.keyCode === 74) || // Ctrl+Shift+J
-        (e.ctrlKey && e.keyCode === 85) // Ctrl+U
-    ) {
-        e.preventDefault();
-        e.stopPropagation();
-        window.location.href = "error.php";
-        return false;
-    }
-}
 
-function disableRightClick(e) {
-    e.preventDefault();
-    return false;
-}
 
-window.onload = function() {
-    window.addEventListener("keydown", blockDevTools);
-    window.addEventListener("contextmenu", disableRightClick);
 
-    // Comprobar si la página se recarga directamente
-    window.onbeforeunload = function() {
-        fetch('logout.php');
-    };
-
-    // Detectar si la página se abrió desde la caché del navegador
-    if (performance.navigation.type === 2) {
-        fetch('logout.php');
-        window.location.href = 'error.php';
-    }
-};
 
 
 // URL del archivo M3U en GitHub
